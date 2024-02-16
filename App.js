297980937ad4d04dbfad3af64c442010bf1576f7
feliciafea <fea2004@gmail.com>
2024-02-16 12:18:15 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Alert, Button, StyleSheet, Text, TextInput, View, Modal, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
+import { FontAwesome6 } from '@expo/vector-icons';
+import { styles } from "./App.styles"
+const DATA = [
+  {
+    id: '1',
+    title: 'Meditation',
+    completed: false,
+    color: "#724E91",
+  },
+  {
+    id: '2',
+    title: 'Coding',
+    completed: false,
+    color: "#E54F6D",
+  },
+  {
+    id: '3',
+    title: 'Journ',
+    completed: false,
+    color: "#F8C630",
+  },
+]
+
+
+export default function App() {
+  const [items, setItems] = useState(DATA);
+  const [text, setText] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+
+  const addNewTodo = () => {
+    let newTodo = {
+      id: items.length + 1,
+      title: text,
+      completed: false,
+      color: "#724E91"
+    }
+  
+    setItems([...items, newTodo]);
+    setText("");
+    setIsModalVisible(false)
+  }
+  const markItemCompleted = (item) => {
+    const itemIndex = items.findIndex(currItem => currItem.id === item.id);
+    if (itemIndex !== -1) {
+      const updatedItems = [...items];
+      updatedItems[itemIndex] = {...items[itemIndex], completed: !item.completed};
+      setItems(updatedItems);
+    }
+  }
+  const TodoItem = (props) => (
+    <TouchableOpacity style = {[styles.item, {backgroundColor: props.item.color}]} onPress={() => markItemCompleted(props.item)}>
+      <Text style ={props.item.completed ? styles.itemTextCompleted : styles.itemText}> {props.item.title}</Text>
+    </TouchableOpacity>
+  )
+
+  const renderAddButton = () => {
+    return (
+      <TouchableOpacity onPress={() => setIsModalVisible(true)}>
+        <View style={styles.icon}>
+          <FontAwesome6 name="add" size={24} color="black" />
+        </View>
+      </TouchableOpacity>
+    )
+  }
+  
+  return (
+    <SafeAreaView style={styles.container}>
+      <Modal visible={isModalVisible} transparent={true} onRequestClose={() => setIsModalVisible(!isModalVisible)}>
+        <View style={styles.centeredView} >
+          <View style={styles.modalView}> 
+            <TextInput style={styles.input} onChangeText={setText} value={text}/>
+            <Button title='AYY Todo' onPress={addNewTodo} />
+          </View>
+        </View>
+      </Modal>
+      <StatusBar style="auto" />
+      <FlatList
+        style={styles.list}
+        data = {items}
+        renderItem={({item}) => <TodoItem item={item} /> }
+        keyExtractor = {item => item.id}
+        ListFooterComponent={renderAddButton}
+        />
+    </SafeAreaView>
+  );
+}
+
+
---
+++ App.styles.js
@@ -0,0 +1,81 @@
+
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Alert, Button, StyleSheet, Text, TextInput, View, Modal, FlatList, SafeAreaView, TouchableOpacity } from 'react-native';
+import { FontAwesome6 } from '@expo/vector-icons';
+
+export const styles = StyleSheet.create({
+       container: {
+         flex: 1,
+         backgroundColor: '#fff',
+         paddingVertical: 58,
+         alignItems: 'center',
+         justifyContent: 'center',
+       },
+       input: {
+         height: 40,
+         width: 200,
+         borderWidth: 1,
+         padding: 10,
+       },
+       list: {
+         alignSelf: 'stretch'
+       },
+       item: {
+         backgroundColor: '#6DB6DD',
+         padding: 20,
+         marginVertical: 8,
+         marginHorizontal: 16,
+         borderRadius: 10,
+       },
+       itemText: {
+         color: '#ffff'
+       },
+       itemTextCompleted: {
+         color: "#ffff",
+         textDecorationLine: 'line-through'
+       },
+       centeredView: {
+         flex: 1,
+         justifyContent: 'center',
+         alignItems: 'center',
+         marginTop: 22,
+       },
+       modalView: {
+         margin: 20,
+         backgroundColor: 'white',
+         borderRadius: 20,
+         padding: 35,
+         alignItems: 'center',
+         shadowColor: '#000',
+         shadowOffset: {
+           width: 0,
+           height: 2,
+         },
+         shadowOpacity: 0.25,
+         shadowRadius: 4,
+         elevation: 5,
+       },
+       icon: {
+         backgroundColor: "white",
+         borderRadius: 100,
+         padding: 10,
+         margin: 5,
+         shadowColor: '#000',
+         shadowOffset: {
+           width: 0,
+           height: 2,
+         },
+         shadowOpacity: 0.25,
+         shadowRadius: 4,
+         elevation: 5,
+         display: "flex",
+         justifyContent: "center",
+         alignItems: "center",
+         alignSelf: "center"
+       },
+       spacer: {
+         height: 20, 
+       },
+     
+     });
